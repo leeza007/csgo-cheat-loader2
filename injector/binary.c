173976390fe52b0785e85ebc169adeff65f6941f
Fabio <50637180+Finguimbrine@users.noreply.github.com>
2020-06-01 22:31:06 -0300

    Update binary.c

--- injector/binary.c
+++ injector/binary.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 // Your DLL as a byte array
-// I need to find a way to download this dll from a server and converting it to a uint8_t array :(
+// I need to find a way to download this dll from a server and convert it to a uint8_t array :(
 // mini tutorial : use the pe2hex python file to get the representation of the dll and put the dll representation here
 
 static const uint8_t binary[] = {
